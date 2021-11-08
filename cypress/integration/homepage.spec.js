873df46eff9bf9e0631536bb47a61c68b9f185f2
Adrian Ross <adriantoddross@gmail.com>
2021-11-08 12:53:09 -0500

    Test if mailto link works successfully

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -9,4 +9,16 @@ describe("Using the header", () => {
     // Confirm we're still on the homepage
     cy.url().should("eq", "http://localhost:3000/");
   });
+
+  it("Clicking e-mail address opens mail client", () => {
+    // // Start on homepage
+    // cy.visit("/");
+
+    // Find e-mail link and make sure it includes href "mailto:trenton@trentoncorp.com"
+    cy.get('[data-testid="email"]').should(
+      "have.attr",
+      "href",
+      "mailto:trenton@trentoncorp.com"
+    );
+  });
 });
