31d39af9daec2b892f51f1c21074886782bc93b6
Adrian Ross <adriantoddross@gmail.com>
2021-11-08 14:37:06 -0500

    Refactor markup

--- components/organisms/Header/index.tsx
+++ components/organisms/Header/index.tsx
@@ -4,27 +4,29 @@ import Link from "next/link";
 export default function Header(): JSX.Element {
   return (
     <header>
-      <Link href="/">
-        <a data-testid="logo">
-          <h1>Trenton Corporation</h1>
-          <Image
-            src="/companyLogo.gif"
-            width="222"
-            height="53"
-            alt="Trenton Corporation logo"
-          />
-        </a>
-      </Link>
+      <div>
+        <Link href="/">
+          <a data-testid="logo">
+            <h1>Trenton Corporation</h1>
+            <Image
+              src="/companyLogo.gif"
+              width="222"
+              height="53"
+              alt="Trenton Corporation logo"
+            />
+          </a>
+        </Link>
 
-      <address>
-        <a data-testid="email" href="mailto:trenton@trentoncorp.com">
-          trenton@trentoncorp.com
-        </a>
+        <address>
+          <a data-testid="email" href="mailto:trenton@trentoncorp.com">
+            trenton@trentoncorp.com
+          </a>
 
-        <a data-testid="address" href="https://goo.gl/maps/R2QPKQwxUqhZg7k68">
-          7700 Jackson Rd, Ann Arbor, MI 48103
-        </a>
-      </address>
+          <a data-testid="address" href="https://goo.gl/maps/R2QPKQwxUqhZg7k68">
+            7700 Jackson Rd, Ann Arbor, MI 48103
+          </a>
+        </address>
+      </div>
     </header>
   );
 }
