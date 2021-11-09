0a4dccf6a84aab3158a49e25685648f13ad92627
Adrian Ross <adriantoddross@gmail.com>
2021-11-09 15:37:44 -0500

    Use ButtonLink component

--- components/organisms/Header/index.tsx
+++ components/organisms/Header/index.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import ButtonLink from "components/molecules/ButtonLink";
+
 export default function Header(): JSX.Element {
   return (
     <header>
@@ -90,15 +92,42 @@ export default function Header(): JSX.Element {
       </div>
 
       <section data-testid="section_one">
-        <h2>
-          Effective, long-lasting anti-corrosion protection above and below
-          ground
-        </h2>
-        <h3>
-          Anti-corrosion wraps, wax coatings, casing fillers, and marine pile
-          protection systems worldwide for oil, gas, water, and chemical
-          processing companies.
-        </h3>
+        <div>
+          <h2>
+            Effective, long-lasting anti-corrosion protection above and below
+            ground
+          </h2>
+          <h3>
+            Anti-corrosion wraps, wax coatings, casing fillers, and marine pile
+            protection systems worldwide for oil, gas, water, and chemical
+            processing companies.
+          </h3>
+
+          <div>
+            <ul>
+              <li>
+                <ButtonLink
+                  className="callUsLink"
+                  href="tel:+17344243600"
+                  text="Call us"
+                />
+              </li>
+
+              <li>
+                <ButtonLink
+                  isNextLink={true}
+                  className="sendAMessageLink"
+                  href="/contact"
+                  text="Send a message"
+                />
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div></div>
+
+        <div></div>
       </section>
     </header>
   );
