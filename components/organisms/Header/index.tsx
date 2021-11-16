805d66fc0f41c4a28b0ed483e7e08a3f9cd37c01
Adrian Ross <adriantoddross@gmail.com>
2021-11-16 16:01:37 -0500

    Lay out navbar like BLO image from stylescape

--- components/organisms/Header/index.tsx
+++ components/organisms/Header/index.tsx
@@ -6,10 +6,28 @@ import MainNavLinks from "components/molecules/MainNavLinks";
 export default function Header(): JSX.Element {
   return (
     <header className="font-sans text-gray-500 ring ring-black ring-opacity-5">
-      <div className="container mx-auto p-4 md:grid md:grid-rows-2 md:grid-cols-2 md:gap-4">
-        <div className=" text-center md:w-32">
+      <address className="hidden md:flex md:flex-row md:flex-nowrap md:justify-center md:items-center md:space-x-4">
+        <a
+          data-testid="email"
+          href="mailto:trenton@trentoncorp.com"
+          className=" not-italic hover:hover:text-black focus:text-red-800 visited:text-purple-600"
+        >
+          trenton@trentoncorp.com
+        </a>
+
+        <a
+          data-testid="address"
+          href="https://goo.gl/maps/R2QPKQwxUqhZg7k68"
+          className=" not-italic hover:hover:text-black focus:text-red-800 visited:text-purple-600"
+        >
+          7700 Jackson Rd, Ann Arbor, MI 48103
+        </a>
+      </address>
+
+      <div className="container mx-auto p-4 md:grid md:grid-cols-1 md:grid-rows-3 md:gap-4">
+        <div>
           <Link href="/">
-            <a data-testid="logo" className="py-4">
+            <a data-testid="logo" className="text-center py-4">
               <h1 className=" text-white text-xs">Trenton Corporation</h1>
               <Image
                 src="/companyLogo.gif"
@@ -20,59 +38,44 @@ export default function Header(): JSX.Element {
             </a>
           </Link>
 
-          <address className="hidden sm:block">
-            <a
-              data-testid="email"
-              href="mailto:trenton@trentoncorp.com"
-              className=" not-italic hover:hover:text-black focus:text-red-800 visited:text-purple-600"
-            >
-              trenton@trentoncorp.com
-            </a>
+          <MainNavLinks
+            className="flex flex-col justify-center items-center font-bold md:font-normal md:flex-row"
+            childClassName="py-4 hover:text-black focus:text-red-800 visited:text-purple-600 md:px-8"
+          />
 
-            <a
-              data-testid="address"
-              href="https://goo.gl/maps/R2QPKQwxUqhZg7k68"
-              className=" not-italic hover:hover:text-black focus:text-red-800 visited:text-purple-600"
-            >
-              7700 Jackson Rd, Ann Arbor, MI 48103
-            </a>
-          </address>
+          <div className="hidden sm:block md:space-x-2">
+            <label htmlFor="search" data-testid="search_label">
+              Search
+            </label>
+            <input
+              type="search"
+              name="query"
+              id="search"
+              data-testid="search_input"
+            />
+          </div>
         </div>
 
-        <div data-testid="language_picker" className="hidden sm:block">
+        <div
+          data-testid="language_picker"
+          className="hidden md:flex md:flex-row md:flex-nowrap md:justify-center md:items-center md:space-x-8"
+        >
           <p>Choose language:</p>
 
-          <div>
+          <div className="md:space-x-2">
             <input type="radio" id="english" name="drone" value="english" />
             <label htmlFor="english">English</label>
           </div>
 
-          <div>
+          <div className="md:space-x-2">
             <input type="radio" id="español" name="drone" value="español" />
             <label htmlFor="español">Español</label>
           </div>
 
-          <div>
+          <div className="md:space-x-2">
             <input type="radio" id="français" name="drone" value="français" />
             <label htmlFor="français">Français</label>
           </div>
-        </div>
-
-        <MainNavLinks
-          className="flex flex-col justify-center items-center font-bold md:font-normal"
-          childClassName="py-4 hover:text-black focus:text-red-800 visited:text-purple-600"
-        />
-
-        <div className="hidden sm:block">
-          <label htmlFor="search" data-testid="search_label">
-            Search
-          </label>
-          <input
-            type="search"
-            name="query"
-            id="search"
-            data-testid="search_input"
-          />
         </div>
       </div>
     </header>
