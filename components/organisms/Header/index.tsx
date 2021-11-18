df7c032a268ff95adb664dc489855222879dbfd0
Adrian Ross <adriantoddross@gmail.com>
2021-11-18 12:37:16 -0500

    Remove CSS grid from header

--- components/organisms/Header/index.tsx
+++ components/organisms/Header/index.tsx
@@ -6,7 +6,7 @@ import MainNavLinks from "components/molecules/MainNavLinks";
 export default function Header(): JSX.Element {
   return (
     <header className="font-sans text-gray-500 ring ring-black ring-opacity-5">
-      <div className="container mx-auto p-4 md:grid md:grid-cols-1 md:grid-rows-1 md:gap-2">
+      <div className="container mx-auto p-4">
         <div className="hidden md:flex md:flex-row md:flex-wrap bg-gray-100 md:p-4">
           <div
             className="hidden md:flex-auto md:flex md:flex-row md:flex-nowrap"
